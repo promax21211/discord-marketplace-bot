d5e1df8bc8974d2be5117f22f4c2a46332bfd345
promax21211 <sanujamansanu@gmail.com>
2025-06-25 23:01:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ config_collection = db["config"]
 # Bot setup
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="-", intents=intents)
+bot.remove_command("help")
 
 # Globals
 whitelisted_roles = ["Admin", "Bot Staff"]
